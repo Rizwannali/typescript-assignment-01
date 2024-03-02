68e7076c87e95590728a98558ab3fa302b6ae6a0
rizwannali <rizzuali601@gmail.com>
2024-03-02 23:26:20 +0500

    first assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("hello world project 01");
+console.log("thank you governor sindh kamran khan tessori ");
+console.log("hello lyari lets change our lifes and others too ");
+console.log("HAPPY CODING");
---
+++ main.ts
@@ -0,0 +1,4 @@
+console.log("hello world project 01")
+console.log("thank you governor sindh kamran khan tessori ")
+console.log("hello lyari lets change our lifes and others too ")
+console.log("HAPPY CODING")
